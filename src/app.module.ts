d075ec445dd64cacd166405631f03d75196b6bce
Igor Tsuker <igorek888rane@gmail.com>
2023-01-25 21:36:19 +0300

    Конфигурация. development.env, production.env. Переменные окружения.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { SequelizeModule } from '@nestjs/sequelize'
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config'
 
 
 @Module({
-	controllers: [AppController],
-	providers: [AppService],
-	imports: [SequelizeModule.forRoot({
+	controllers: [],
+	providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath:`.${process.env.NODE_ENV}.env`
+		}),
+		SequelizeModule.forRoot({
 		dialect: 'postgres',
-		host: 'localhost',
-		port: 5433,
-		username: 'postgres',
-		password: 'root',
-		database: 'nest-practice',
+		host: process.env.POSTGRES_HOST,
+		port: Number(process.env.POSTGRES_PORT),
+		username: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DB,
 		models: [],
 		autoLoadModels: true
-	})
+	}), UsersModule
 
 	]
 })
